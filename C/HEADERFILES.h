0d83086432b86892cd5b7a82296a821cc7193ccc
Kishu4code <96542183+Kishu4code@users.noreply.github.com>
2025-04-29 10:27:37 +0530

    Add files via upload

---
+++ C/HEADERFILES.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+
+#include "LOGIN_USER.h"
+#include "REGISTER_USER.h"
+#include "PASSWORD_SECURITY.h"
+#include "FUNCTION.h"
+#include "NOTEPAD.h"
+#include "NOTEPAD_HELP.h"
+#include "NOTEPAD_DEMO.h"
+#include "NOTEPAD_ENTERTEXT.h"
+#include "NOTEPAD_DISPLAY.h"
+#include "NOTEPAD_ADDTEXT.h"
